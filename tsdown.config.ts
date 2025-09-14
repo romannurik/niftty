3f6991dc65562916d0e1c20fed936405e7685282
Roman Nurik <roman@nurik.net>
2025-09-13 20:42:50 -0400

    more meta changes

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from "tsdown";
 
+// TODO: outputOptions.legalComments isn't working, so manually add license banner.
+const LICENSE_BANNER = `
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+`;
+
 export default defineConfig({
   entry: ["./src/index.ts"],
   format: ["esm", "cjs"],
   target: ["es2022"],
-  // clean: true,
+  clean: true,
   minify: true,
-  outputOptions: {
-    legalComments: 'inline', // todo: get this working
-  },
+  banner: LICENSE_BANNER,
   dts: true,
   sourcemap: true,
   platform: "node",
